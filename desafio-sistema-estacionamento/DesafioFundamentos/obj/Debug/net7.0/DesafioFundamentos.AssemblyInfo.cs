cf491e58ee10eb5eca660db300a26d45cb27bc73
carlos <cmsilva1601@gmail.com>
2024-10-06 12:27:03 -0300

    Desafio Concluído

--- desafio-sistema-estacionamento/DesafioFundamentos/obj/Debug/net7.0/DesafioFundamentos.AssemblyInfo.cs
+++ desafio-sistema-estacionamento/DesafioFundamentos/obj/Debug/net7.0/DesafioFundamentos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DesafioFundamentos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9953243d9777a02f18aace2fa1472d4675f996aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0185a6354148e022bbc0159e088a1f300fe6ab02")]
 [assembly: System.Reflection.AssemblyProductAttribute("DesafioFundamentos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DesafioFundamentos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
